5e8ae80a88700bc94dfd677b175a5f989463a251
Riamu <liamhartery@gmail.com>
2016-10-03 18:37:45 -0300

    Uploaded Initial libGDX files

---
+++ desktop/src/com/voltahackathon001/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.voltahackathon001.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.voltahackathon001.game.CaveGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CaveGame(), config);
+	}
+}
