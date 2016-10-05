fad8e36d17ae939cd60e03c8efe6873fce29ad64
Riamu <liamhartery@gmail.com>
2016-10-05 20:22:27 -0300

    Did some stuff

--- core/src/com/voltahackathon001/game/entities/Player.java
+++ core/src/com/voltahackathon001/game/entities/Player.java
@@ -5,31 +5,41 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.voltahackathon001.game.screens.PlayScreen;
 
 public class Player extends Sprite{
+    // physics constants
     private final float GRAVITY = 1000f;
     private float ACCELERATION = 1000;
     private final float TOP_SPEED = 600;
-    private float MAX_ACCEL = 5000;
+    private final float MAX_ACCEL = 5000;
 
+    private float ANIMATION_SPEED = 0.1f;
     private float jumptimer = 0;
 
     private float oldPosX,oldPosY;
     private Vector2 pos,vel,accel;
     private Animation animation,jumpleft,jumpright,runleft,runright,idle;
-    private TextureAtlas atlas;
-    private static Texture texture = new Texture(Gdx.files.internal("baddie16.png"));
+    private TextureAtlas jumpermanAtlas;
     private PlayScreen screen;
     private boolean isJumping = false;
-    private boolean collisionX,collisionY;
-    public Player(float x, float y, PlayScreen screen){
-        super(texture);
-        // atlas = new TextureAtlas(gdx.files.internal("atlas.atlas"));
+    private boolean collisionX,collisionY,flying;
+
+    // Constructor
+    public Player(float x, float y, PlayScreen screen, TextureAtlas atlas){
+        // call Sprite constructor with a texureRegion param
+        super(atlas.getRegions().get(0));
+        jumpermanAtlas = atlas;
+        setUpAnimations();
+
+        // create our pos vector and set position to x,y
         pos = new Vector2(x,y);
         setX(pos.x);
         setY(pos.y);
+
+        // instantiate vel and accel
         vel = new Vector2(0,0);
         accel = new Vector2(0,0);
         this.screen = screen;
@@ -38,30 +48,48 @@ public class Player extends Sprite{
         isJumping=true;
         jumptimer = 0;
     }
+
+    // updates EVERYTHING
     public void update(float delta){
+        // jumping timer code
         jumptimer+=delta;
         if(jumptimer>0.25){
             isJumping = false;
         }
+
         // apply gravity acceleration
         accel.y = -GRAVITY;
         accel.x = 0;
+
+        // if a is pressed we accelerate in x towards the left
         if(screen.aPressed){
             accel.x -=ACCELERATION;
+            if(!flying) {
+                animation = runleft;
+            }
+
         }
+        // if d is pressed we accelerate in x towards the right
         if(screen.dPressed){
             accel.x += ACCELERATION;
+            if(!flying) {
+                animation = runright;
+            }
+
         }
+        // cap our accel
         if(accel.x>MAX_ACCEL){
             accel.x = MAX_ACCEL;
         }
         if(accel.x<-MAX_ACCEL){
             accel.x = -MAX_ACCEL;
         }
+
         // apply acceleration to velocity
         vel.y+=accel.y*delta;
         vel.x += accel.x*delta;
 
+        // decay our x velocity stuff
         if(vel.x>0){
             vel.x-= 10;
         }
@@ -69,15 +97,10 @@ public class Player extends Sprite{
             vel.x+= 10;
         }
 
-
-        // Cap velocity in both directions to 10
-        // TODO TEST ME (maybe have to be combined velocity
-
-
+        // cap velocity in both direction
         if(vel.x>TOP_SPEED){
             vel.x = TOP_SPEED;
         }
-
         if(vel.y>TOP_SPEED){
             vel.y = TOP_SPEED;
         }
@@ -87,14 +110,15 @@ public class Player extends Sprite{
         if(vel.x<-TOP_SPEED){
             vel.y = -TOP_SPEED;
         }
-        if(Math.abs(vel.y)<0.1f){
+
+        // if our velocity is sufficiently low, just stop movement
+        // this prevents gliding on surfaces
+        if(Math.abs(vel.y)<1f){
             vel.y=0;
         }
-        if(Math.abs(vel.x)<0.1f){
+        if(Math.abs(vel.x)<1f){
             vel.x=0;
         }
-        // fix with collision detection
-        // TODO TEST ME
 
         // move in x
         oldPosX = pos.x;
@@ -110,14 +134,20 @@ public class Player extends Sprite{
         if(collisionX){
             pos.x = oldPosX;
             if(isJumping&&!collisionY){
+                // wall jumping from a right wall
                 if(vel.x>0){
                     vel.x = -500;
                     vel.y = 1000;
                     isJumping = false;
+                    animation = jumpleft;
+                    flying = true;
+                // wall jumping from a left wall
                 }else if(vel.x<0){
                     vel.x = 500;
                     vel.y = 1000;
                     isJumping = false;
+                    animation = jumpright;
+                    flying = true;
                 }
             }else{
                 vel.x = 0;
@@ -130,6 +160,9 @@ public class Player extends Sprite{
         // check collision in y
         if(vel.y<0){ // going down (yelling timber)
             collisionY = collidesBottom();
+            if(collisionY){
+                flying = false;
+            }
 
         }else if(vel.y>0){ // going up (whispering timber)
             collisionY = collidesTop();
@@ -171,13 +204,48 @@ public class Player extends Sprite{
             return true;
         }else return false;
     }
+
+    // just sets up animations, pretty self explanatory
     private void setUpAnimations(){
-        // Set up the different animations here
+        //animation,jumpleft,jumpright,runleft,runright,idle;
+
+        // set jumpleft animation to anew animation with ANIMATION_SPEED
+        // and the atlas regions defined by our findRegion.
+        jumpleft = new Animation(ANIMATION_SPEED,
+                jumpermanAtlas.findRegion("jumperman13")
+                );
+
+        jumpright = new Animation(ANIMATION_SPEED,
+                jumpermanAtlas.findRegion("jumperman12")
+                );
+
+        runleft = new Animation(ANIMATION_SPEED,
+                jumpermanAtlas.findRegion("jumperman06"),
+                jumpermanAtlas.findRegion("jumperman07"),
+                jumpermanAtlas.findRegion("jumperman08"),
+                jumpermanAtlas.findRegion("jumperman09"),
+                jumpermanAtlas.findRegion("jumperman10"),
+                jumpermanAtlas.findRegion("jumperman11")
+                );
+        runright = new Animation(0.1f,
+                jumpermanAtlas.findRegion("jumperman00"),
+                jumpermanAtlas.findRegion("jumperman01"),
+                jumpermanAtlas.findRegion("jumperman02"),
+                jumpermanAtlas.findRegion("jumperman03"),
+                jumpermanAtlas.findRegion("jumperman04"),
+                jumpermanAtlas.findRegion("jumperman05")
+                );
+        animation = runright;
+    }
+    public Animation getAnimation(){
+        return animation;
     }
     public float getVelX(){
         return vel.x;
     }
     public float getVelY() { return vel.y;}
+
+    // returns a string of player's stats for testing
     public String getVitals(){
         return(
                 "VelX: "+vel.x+"\n"+
